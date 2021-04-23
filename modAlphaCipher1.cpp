df0e99d504e618b3c3875e4f481fa1fbf50bec0a
CloudMaks <81799531+CloudMaks@users.noreply.github.com>
2021-04-23 19:45:54 +0300

    Update modAlphaCipher1.cpp

--- modAlphaCipher1.cpp
+++ modAlphaCipher1.cpp
@@ -1,4 +1,5 @@
-#include "modAlphaCipher.h"
+#include "modAlphaCipher1.h"
+
 modAlphaCipher::modAlphaCipher(const string& skey)
 {
     locale loc("ru_RU.UTF-8"); // русская локаль для корректной смены регистра
@@ -22,7 +23,8 @@ string modAlphaCipher::encrypt(const string& open_text)
 string modAlphaCipher::decrypt(const string& cipher_text)
 {
     vector<int> work = convert(cipher_text);
-    for(unsigned i = 0; i < work.size(); i++) {
+    for(unsigned i = 0; i < work.size(); i++) 
+    {
         work[i] = (work[i] + alphaNum.size() - key[i % key.size()]) % alphaNum.size();
     }
     return convert(work);
@@ -34,7 +36,8 @@ inline vector<int> modAlphaCipher::convert(const string& s)
     locale loc("ru_RU.UTF-8"); // русская локаль для корректной смены регистра
     wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; //кодек UTF-8
     wstring ws = codec.from_bytes(s); // перекодируем
-    for (unsigned i = 0; i<ws.size(); i++) {
+    for (unsigned i = 0; i<ws.size(); i++) 
+    {
         result.push_back(alphaNum[ws[i]]);
     }
     return result;
@@ -47,7 +50,8 @@ inline string modAlphaCipher::convert(const vector<int>& v)
     wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; //кодек UTF-8
     wstring ws = codec.from_bytes(numAlpha);
     wstring result_s = codec.from_bytes("");
-    for (unsigned i = 0; i<v.size(); i++) {
+    for (unsigned i = 0; i<v.size(); i++) 
+    {
         result_s.push_back(ws[v[i]]);
     }
     result = codec.to_bytes(result_s);
@@ -59,7 +63,8 @@ inline string modAlphaCipher::convert(const vector<int>& v)
     wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; //кодек UTF-8
     wstring ws = codec.from_bytes(numAlpha);
     wstring result_s = codec.from_bytes("");
-    for (unsigned i=0; i<v.size(); i++) {
+    for (unsigned i=0; i<v.size(); i++) 
+    {
         result_s.push_back(ws[v[i]]);
     }
     result = codec.to_bytes(result_s);
