9fd1df67581319ce5f20179c1ed6f0b81c4971cd
CloudMaks <81799531+CloudMaks@users.noreply.github.com>
2021-04-23 19:43:37 +0300

    Update and rename modAlphaCipher.cpp to modAlphaCipher1.cpp

--- modAlphaCipher1.cpp
+++ modAlphaCipher1.cpp
@@ -4,7 +4,7 @@ modAlphaCipher::modAlphaCipher(const string& skey)
     locale loc("ru_RU.UTF-8"); // русская локаль для корректной смены регистра
     wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; //кодек UTF-8
     wstring ws = codec.from_bytes(numAlpha); // перекодируем
-    for (unsigned i=0; i<ws.size(); i++) {
+    for (unsigned i = 0; i < ws.size(); i++) {
         alphaNum[ws[i]]=i;
     }
     key = convert(skey);
@@ -13,7 +13,7 @@ modAlphaCipher::modAlphaCipher(const string& skey)
 string modAlphaCipher::encrypt(const string& open_text)
 {
    vector<int> work = convert(open_text);
-    for(unsigned i=0; i < work.size(); i++) {
+    for(unsigned i = 0; i < work.size(); i++) {
         work[i] = (work[i] + key[i % key.size()]) % alphaNum.size();
     }
     return convert(work);
@@ -22,7 +22,7 @@ string modAlphaCipher::encrypt(const string& open_text)
 string modAlphaCipher::decrypt(const string& cipher_text)
 {
     vector<int> work = convert(cipher_text);
-    for(unsigned i=0; i < work.size(); i++) {
+    for(unsigned i = 0; i < work.size(); i++) {
         work[i] = (work[i] + alphaNum.size() - key[i % key.size()]) % alphaNum.size();
     }
     return convert(work);
@@ -34,7 +34,7 @@ inline vector<int> modAlphaCipher::convert(const string& s)
     locale loc("ru_RU.UTF-8"); // русская локаль для корректной смены регистра
     wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; //кодек UTF-8
     wstring ws = codec.from_bytes(s); // перекодируем
-    for (unsigned i=0; i<ws.size(); i++) {
+    for (unsigned i = 0; i<ws.size(); i++) {
         result.push_back(alphaNum[ws[i]]);
     }
     return result;
@@ -47,7 +47,7 @@ inline string modAlphaCipher::convert(const vector<int>& v)
     wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; //кодек UTF-8
     wstring ws = codec.from_bytes(numAlpha);
     wstring result_s = codec.from_bytes("");
-    for (unsigned i=0; i<v.size(); i++) {
+    for (unsigned i = 0; i<v.size(); i++) {
         result_s.push_back(ws[v[i]]);
     }
     result = codec.to_bytes(result_s);
